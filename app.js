fb56d065e580c683f17fa648d49adac344a4ed8f
bhumik4mishra <bhumikamishra49@gmail.com>
2025-09-16 01:20:45 +0530

    Add files via upload
    updated

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 
-let b=[];
-let a;
+btn=document.querySelector(".q");
+b=document.querySelector("button");
+inp=document.querySelector("input");
+e=document.querySelector("#h");
+l=document.querySelectorAll("ul li");
+dbtn=document.querySelectorAll(".q");
 
-while(true){
-    a=prompt("enter ur choice");
-if(a=="quit"||a=="3"){
-    break;
+
+
+
+b.addEventListener("click",function(){
+s=document.createElement("li");
+s.textContent=inp.value;
+n=e.appendChild(s);
+dbtn=document.createElement("button");
+dbtn.innerText="Delete";
+n.append(dbtn);
+
+
+
+});
+
+e.addEventListener("click",function(event){
+
+let y=event.target.nodeName;
+if(y=="BUTTON"){
+    let z=event.target.parentElement;
+    z.remove();
+}
+else{
+     console.log("dont delete");
 }
 
-if(a=="1"){
-let  j=prompt("enter task to add");
-b.unshift(j);
 
-console.log(" task added");
-console.log("-----------------");
-for(task of b){
-    console.log(task);
-}
-console.log("-----------------");
 
-}
-else if(a=="2"){
-b.pop();
-console.log("removed last task");
-console.log("-----------------");
-}
-
-else if(a=="4"){
-    console.log("TASKS");
-for(let i=0;i<b.length;i++){
-console.log(b[i]);
-
-    
-}
+});
 
 
 
-}}
+
